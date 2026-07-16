78628a4e3b026eb5c015827fd8c81725682991d1
Muhammed Emin Yılmaz <162921755+KelvinBrendon53@users.noreply.github.com>
2026-07-16 20:23:48 +0300

    App.py

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("📦 STOK LİSTESİ")
 
 # Sadece Google Sheets URL'ini yapıştır
-SHEET_URL = "https://docs.google.com/spreadsheets/d/1UjCSBo1DmOoaFm7iLtB3CEBJc63Sf7xDDSKYSONhb7I/edit?gid=0#gid=0"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1Kbzpbu-mxaXZmY52qXVoj0nxF_X4tO4l/edit?gid=1034042521#gid=1034042521"
 
 # Veriyi oku (API anahtarı gerekmez)
 df = pd.read_csv(SHEET_URL.replace("/edit?gid=0#gid=0", "/export?format=csv&gid=0"))
