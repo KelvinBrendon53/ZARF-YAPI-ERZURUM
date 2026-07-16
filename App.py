a9b0fc3d41b73ee33bb6d3731c39e38001a94681
Muhammed Emin Yılmaz <162921755+KelvinBrendon53@users.noreply.github.com>
2026-07-16 20:14:29 +0300

    Add files via upload

---
+++ App.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+st.title("📦 STOK LİSTESİ")
+
+# Sadece Google Sheets URL'ini yapıştır
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1UjCSBo1DmOoaFm7iLtB3CEBJc63Sf7xDDSKYSONhb7I/edit?gid=0#gid=0"
+
+# Veriyi oku (API anahtarı gerekmez)
+df = pd.read_csv(SHEET_URL.replace("https://docs.google.com/spreadsheets/d/1UjCSBo1DmOoaFm7iLtB3CEBJc63Sf7xDDSKYSONhb7I/edit?gid=0#gid=0"))
+
+st.write("Mevcut Stok Durumun:")
+st.dataframe(df)
