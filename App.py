f5808c64b418a839b18c7209818a4633a4e4d895
Muhammed Emin Yılmaz <162921755+KelvinBrendon53@users.noreply.github.com>
2026-07-16 20:17:18 +0300

    App.py

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ st.title("📦 STOK LİSTESİ")
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1UjCSBo1DmOoaFm7iLtB3CEBJc63Sf7xDDSKYSONhb7I/edit?gid=0#gid=0"
 
 # Veriyi oku (API anahtarı gerekmez)
-df = pd.read_csv(SHEET_URL.replace("https://docs.google.com/spreadsheets/d/1UjCSBo1DmOoaFm7iLtB3CEBJc63Sf7xDDSKYSONhb7I/edit?gid=0#gid=0"))
+df = pd.read_csv(SHEET_URL.replace("/edit?gid=0#gid=0", "/export?format=csv&gid=0"))
 
 st.write("Mevcut Stok Durumun:")
 st.dataframe(df)
