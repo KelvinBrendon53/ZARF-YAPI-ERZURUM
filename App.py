d03b34e241d544fbbef38f2c95aa632496d01b84
Muhammed Emin Yılmaz <162921755+KelvinBrendon53@users.noreply.github.com>
2026-07-16 20:32:25 +0300

    App.py

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("📦 Zarf Yapı - Depo Stok")
 
 DOSYA_ID = "1Kbzpbu-mxaXZmY52qXVoj0nxF_X4tO4l"
-CSV_URL = f"https://docs.google.com/spreadsheets/d//{DOSYA_ID}/export?format=csv&gid=0"
+CSV_URL = f"https://docs.google.com/spreadsheets/d//{DOSYA_ID}/export?format=csv&gid=1034042521"
 
 # Veriyi çek
 try:
