6c6eeaa3150ea88b3f3ea11f8522f54dafaa8c9d
Muhammed Emin Yılmaz <162921755+KelvinBrendon53@users.noreply.github.com>
2026-07-22 11:43:18 +0300

    App.py

--- App.py
+++ App.py
@@ -32,15 +32,17 @@ def load_data():
     df = pd.read_csv(CSV_URL).fillna("") 
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
     
-    # Görseldeki #,##0 formatının Python karşılığı (Binlikleri nokta ile ayırma)
     for col in df.columns:
         if any(kelime in col for kelime in ['STOK', 'GELEN', 'ÇIKIŞ']):
             def format_excel_style(val):
                 temiz = str(val).replace('.', '').replace(',', '').strip()
-                if temiz.isdigit():
+
+                if temiz.isdigit() and len(temiz) > 3:  
                     return f"{int(temiz):,}".replace(',', '.')
                 return val
             df[col] = df[col].apply(format_excel_style)
+            
+    return df
             
     return df
 df = load_data()
